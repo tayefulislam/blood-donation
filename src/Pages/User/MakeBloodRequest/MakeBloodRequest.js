15f24d325cacbaf343efebaa2ede424f9aaaab65
tayeful912 <kazitayefulislam12@gmail.com>
2022-06-05 00:00:09 +0600

    make request compotent complete with data processing

--- src/Pages/User/MakeBloodRequest/MakeBloodRequest.js
+++ src/Pages/User/MakeBloodRequest/MakeBloodRequest.js
@@ -1,6 +1,48 @@
 import React from 'react';
+import { format, parseISO } from 'date-fns';
+
 
 const MakeBloodRequest = () => {
+
+
+    const handleRequest = (event) => {
+
+        event.preventDefault()
+        // convet time
+
+
+        const getTime = event?.target?.time?.value;
+        const timeString = getTime + ':00'
+
+        const time = new Date('1970-01-01T' + timeString + 'Z')
+            .toLocaleTimeString('en-US',
+                { timeZone: 'UTC', hour12: true, hour: 'numeric', minute: 'numeric' }
+            );
+
+        // comvert date
+
+        const getDate = parseISO(event.target.date.value);
+
+        const stringDate = format(getDate || new Date(), 'PP')
+
+        console.log(stringDate)
+
+        const patient = event.target.patient.value;
+        const medical = event.target.medical.value;
+        const number = event.target.number.value;
+        const unit = event.target.unit.value;
+        const group = event.target.group.value;
+
+
+        const type = event.target.type.value;
+        const district = event.target.district.value;
+        const area = event.target.area.value;
+
+        const bloodRequest = { patient, medical, number, unit, group, date: stringDate, time: time, type, district, area }
+
+        console.log(bloodRequest)
+
+    }
     return (
         <div className='flex justify-center items-center'>
 
@@ -11,14 +53,14 @@ const MakeBloodRequest = () => {
                 <div className='card-body'>
 
 
-                    <form >
+                    <form onSubmit={handleRequest} >
 
                         <div class="form-control w-full max-w-xs">
                             <label class="label">
                                 <span class="label-text">Patient Name :</span>
 
                             </label>
-                            <input type="text" placeholder="Patient Name" name='patient' class="input input-bordered input-error w-full max-w-xs" />
+                            <input type="text" required placeholder="Patient Name" name='patient' class="input input-bordered input-error w-full max-w-xs" />
                             <label class="label">
 
                             </label>
@@ -28,7 +70,7 @@ const MakeBloodRequest = () => {
                                 <span class="label-text">Medical Name:</span>
 
                             </label>
-                            <input type="text" placeholder="Medical Name" name='medical' class="input input-bordered input-error w-full max-w-xs" />
+                            <input type="text" required placeholder="Medical Name" name='medical' class="input input-bordered input-error w-full max-w-xs" />
                             <label class="label">
 
                             </label>
@@ -38,7 +80,7 @@ const MakeBloodRequest = () => {
                                 <span class="label-text">Phone Number:</span>
 
                             </label>
-                            <input type="number" placeholder="Phone Number" name='number' class="input input-bordered input-error w-full max-w-xs" />
+                            <input type="number" required placeholder="Phone Number" name='number' class="input input-bordered input-error w-full max-w-xs" />
                             <label class="label">
 
                             </label>
@@ -48,7 +90,7 @@ const MakeBloodRequest = () => {
                                 <span class="label-text">Units/Blood Bag?</span>
 
                             </label>
-                            <input type="text" placeholder="Units/Blood Bag" name='unit' class="input input-bordered input-error w-full max-w-xs" />
+                            <input type="number" required placeholder="Units/Blood Bag" name='unit' class="input input-bordered input-error w-full max-w-xs" />
                             <label class="label">
 
                             </label>
@@ -59,7 +101,7 @@ const MakeBloodRequest = () => {
 
                             </label>
 
-                            <select name='group' class="select select-secondary w-full max-w-xs">
+                            <select name='group' required class="select select-error w-full max-w-xs">
                                 <option disabled selected>Select Blood Group</option>
                                 <option value='A+'>A+ (A POSITIVE)</option>
                                 <option value='A-'>A- (A NEGATIVE)</option>
@@ -82,7 +124,7 @@ const MakeBloodRequest = () => {
                                 <span class="label-text">Blood Donation Date :</span>
 
                             </label>
-                            <input type="date" placeholder="Blood Donation Date " name='date' class="input input-bordered input-error w-full max-w-xs" />
+                            <input type="date" required placeholder="Blood Donation Date " name='date' class="input input-bordered input-error w-full max-w-xs" />
                             <label class="label">
 
                             </label>
@@ -92,7 +134,7 @@ const MakeBloodRequest = () => {
                                 <span class="label-text">Blood Donation Time :</span>
 
                             </label>
-                            <input type="time" placeholder="Blood Donation Time" name='time' class="input input-bordered input-error w-full max-w-xs" />
+                            <input type="time" required placeholder="Blood Donation Time" name='time' class="input input-bordered input-error w-full max-w-xs" />
                             <label class="label">
 
                             </label>
@@ -102,7 +144,7 @@ const MakeBloodRequest = () => {
                                 <span class="label-text">Requirement :</span>
 
                             </label>
-                            <select name='group' class="select select-secondary w-full max-w-xs">
+                            <select name='type' class="select select-error w-full max-w-xs">
 
                                 <option value='blood'>Blood</option>
                                 <option value='platelets'>Platelets</option>
@@ -120,7 +162,7 @@ const MakeBloodRequest = () => {
                             </label>
 
 
-                            <select name='group' class="select select-secondary w-full max-w-xs">
+                            <select name='district' required class="select select-error w-full max-w-xs">
 
                                 <option value='Dhaka'>Dhaka</option>
                                 <option value='Brahmanbaria'>Brahmanbaria</option>
@@ -134,14 +176,14 @@ const MakeBloodRequest = () => {
                                 <span class="label-text">Area :</span>
 
                             </label>
-                            <textarea type="text" placeholder="Area" name='area' class="input input-bordered input-error w-full max-w-xs" />
+                            <textarea type="text" required placeholder="Area" name='area' class="input input-bordered input-error w-full max-w-xs" />
                             <label class="label">
 
                             </label>
                         </div>
 
                         <div className='flex justify-center items-center'>
-                            <input className='btn' type="button" value="Submit" />
+                            <input className='btn' type="submit" value="Submit" />
                         </div>
 
 
