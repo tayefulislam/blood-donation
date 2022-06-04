fafca98294def4f9f27ebf2fcb52ac658a2860e9
tayeful912 <kazitayefulislam12@gmail.com>
2022-06-04 20:47:24 +0600

    Header added and MakeBloodRequest component added

---
+++ src/Pages/User/MakeBloodRequest/MakeBloodRequest.js
@@ -0,0 +1,96 @@
+import React from 'react';
+
+const MakeBloodRequest = () => {
+    return (
+        <div className=''>
+
+            <div className=' flex justify-center items-center w-full'>
+
+                <form >
+
+                    <div class="form-control w-full max-w-xs">
+                        <label class="label">
+                            <span class="label-text">Patient Name :</span>
+
+                        </label>
+                        <input type="text" placeholder="" name='patient' class="input input-bordered input-error w-full max-w-xs" />
+                        <label class="label">
+
+                        </label>
+                    </div>
+                    <div class="form-control w-full max-w-xs">
+                        <label class="label">
+                            <span class="label-text">Patient Name :</span>
+
+                        </label>
+                        <input type="text" placeholder="" name='patient' class="input input-bordered input-error w-full max-w-xs" />
+                        <label class="label">
+
+                        </label>
+                    </div>
+                    <div class="form-control w-full max-w-xs">
+                        <label class="label">
+                            <span class="label-text">Patient Name :</span>
+
+                        </label>
+                        <input type="text" placeholder="" name='patient' class="input input-bordered input-error w-full max-w-xs" />
+                        <label class="label">
+
+                        </label>
+                    </div>
+                    <div class="form-control w-full max-w-xs">
+                        <label class="label">
+                            <span class="label-text">Patient Name :</span>
+
+                        </label>
+                        <input type="text" placeholder="" name='patient' class="input input-bordered input-error w-full max-w-xs" />
+                        <label class="label">
+
+                        </label>
+                    </div>
+                    <div class="form-control w-full max-w-xs">
+                        <label class="label">
+                            <span class="label-text">Patient Name :</span>
+
+                        </label>
+                        <input type="text" placeholder="" name='patient' class="input input-bordered input-error w-full max-w-xs" />
+                        <label class="label">
+
+                        </label>
+                    </div>
+                    <div class="form-control w-full max-w-xs">
+                        <label class="label">
+                            <span class="label-text">Patient Name :</span>
+
+                        </label>
+                        <input type="text" placeholder="" name='patient' class="input input-bordered input-error w-full max-w-xs" />
+                        <label class="label">
+
+                        </label>
+                    </div>
+                    <div class="form-control w-full max-w-xs">
+                        <label class="label">
+                            <span class="label-text">Patient Name :</span>
+
+                        </label>
+                        <input type="text" placeholder="" name='patient' class="input input-bordered input-error w-full max-w-xs" />
+                        <label class="label">
+
+                        </label>
+                    </div>
+
+
+
+                </form>
+
+
+
+            </div>
+
+
+
+        </div>
+    );
+};
+
+export default MakeBloodRequest;
