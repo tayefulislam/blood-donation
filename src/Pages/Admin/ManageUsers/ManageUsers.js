de04701869868b3ced36d42811c220400512bd25
tayefulislam <kazitayefulislam12@gmail.com>
2022-10-13 01:19:14 +0600

    Manager user ui design almost complete

--- src/Pages/Admin/ManageUsers/ManageUsers.js
+++ src/Pages/Admin/ManageUsers/ManageUsers.js
@@ -1,82 +1,36 @@
-import React from 'react';
-import { useQuery } from 'react-query';
-
+import React from "react";
+import { useQuery } from "react-query";
 
 const ManageUsers = () => {
+  const usersUrl = `http://localhost:5000/admin/users`;
+  const { isLoading, error, data } = useQuery("allusers", () =>
+    fetch(usersUrl).then((res) => res.json())
+  );
 
-    const usersUrl = `http://localhost:5000/admin/users`;
-    const { isLoading, error, data } = useQuery('allusers', () => fetch(usersUrl).then(res => res.json()));
+  console.log(data);
 
-    console.log(data);
-
-
-
-
-
-
-
-
-    return (
-        <div class="overflow-x-auto w-full">
-            <table class="table w-full">
-
-                <thead>
-                    <tr>
-                        <th>
-                            <label>
-                                <input type="checkbox" class="checkbox" />
-                            </label>
-                        </th>
-                        <th>Name</th>
-                        <th>Blood Group</th>
-                        <th>Phone Number</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-
-                    {data.map(user => <tr>
-                        <th>
-                            <label>
-                                <input type="checkbox" class="checkbox" />
-                            </label>
-                        </th>
-                        <td>
-                            <div class="flex items-center space-x-3">
-
-                                <div>
-                                    <div class="font-bold">{user?.name}</div>
-                                    <div class="text-sm opacity-50">{user?.district}</div>
-                                </div>
-                            </div>
-                        </td>
-
-                        <td>
-                            <div class="font-bold">{user?.group}</div>
-                        </td>
-                        <td>
-                            <div class="font-bold">{user?.number}</div>
-                        </td>
-
-
-                        <td>
-                            Zemlak, Daniel and Leannon <br></br>
-
-                            <span class="badge badge-ghost badge-sm">Desktop Support Technician</span>
-                        </td>
-                        <td>Purple</td>
-                        <th>
-                            <button class="btn btn-ghost btn-xs">details</button>
-                        </th>
-                    </tr>)}
-
-
-                </tbody>
-
-
-            </table>
-        </div>
-    );
+  return (
+    <div>
+      <h1 className="text-center">Total User : {data?.length}</h1>
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-2 mx-2">
+        {data?.map((user, index) => (
+          <div class="card w-full bg-neutral text-neutral-content">
+            <div class="card-body items-center text-center">
+              <h2 class="card-title">{user?.name}</h2>
+              <h2 class="card-title">{user?.group}</h2>
+              <p>
+                {user?.number} / {user?.district}
+              </p>
+              <div class="card-actions justify-end">
+                <button class="btn btn-primary">Admin</button>
+                <button class="btn btn-ghost">Deny</button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default ManageUsers;
