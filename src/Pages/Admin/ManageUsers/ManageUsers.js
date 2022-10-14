2da3561e077bf383417f734507784f230f78c4e9
tayefulislam <kazitayefulislam12@gmail.com>
2022-10-14 20:35:17 +0600

    Add Manager user with bug

--- src/Pages/Admin/ManageUsers/ManageUsers.js
+++ src/Pages/Admin/ManageUsers/ManageUsers.js
@@ -1,19 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from "react-query";
+import axios from "axios";
+
+import Backdrop from "@mui/material/Backdrop";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import Fade from "@mui/material/Fade";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Loading from "../../Shared/Loading/Loading";
 
 const ManageUsers = () => {
-  const usersUrl = `https://blooddonationmvc.onrender.com/api/v1/donors/admin/users`;
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+  };
+
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const [userModal, setUser] = useState("");
+
+  const usersUrl = `http://localhost:5000/api/v1/donors/admin/users`;
   const { isLoading, error, data } = useQuery("allusers", () =>
     fetch(usersUrl).then((res) => res.json())
   );
 
-  console.log(data);
+  // console.log(data);
+
+  // console.log(data);
+
+  const makeAdmin = (userEmail) => {
+    const url = `http://localhost:5000/api/v1/donors/${userEmail}`;
+
+    axios.patch(url).then(function (response) {
+      console.log(response);
+    });
+  };
+
+  const handleModal = (user) => {
+    handleOpen();
+    console.log(user);
+    setUser(user);
+  };
+
+  if (isLoading) {
+    return <Loading></Loading>;
+  }
 
   return (
     <div>
       <h1 className="text-center">Total User : {data?.length}</h1>
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-2 mx-2">
-        {data?.map((user, index) => (
+        {data?.map((user) => (
           <div class="card w-full bg-neutral text-neutral-content">
             <div class="card-body items-center text-center">
               <h2 class="card-title">{user?.name}</h2>
@@ -22,13 +69,47 @@ const ManageUsers = () => {
                 {user?.number} / {user?.district}
               </p>
               <div class="card-actions justify-end">
-                <button class="btn btn-primary">Admin</button>
-                <button class="btn btn-ghost">Deny</button>
+                <button
+                  onClick={() => makeAdmin(user?.email)}
+                  class="btn btn-primary"
+                >
+                  Admin
+                </button>
+                <button class="btn btn-ghost" onClick={() => handleModal(user)}>
+                  Details
+                </button>
               </div>
             </div>
           </div>
         ))}
       </div>
+
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <Box sx={style}>
+            <Typography id="transition-modal-title" variant="h6" component="h2">
+              {userModal?.name}
+            </Typography>
+            <Typography id="transition-modal-description" sx={{ mt: 2 }}>
+              <h1>Group : {userModal?.group}</h1>
+              <h1>Number : {userModal?.number}</h1>
+              <h1>District : {userModal?.district}</h1>
+              <h3>Area : {userModal?.area}</h3>
+              <h3>Email : {userModal?.email}</h3>
+            </Typography>
+          </Box>
+        </Fade>
+      </Modal>
     </div>
   );
 };
--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { useQuery } from "react-query";
 
 import { useAuthState } from "react-firebase-hooks/auth";
 // import { Link } from 'react-router-dom';
@@ -10,6 +11,17 @@ import auth from "../../firebase.init";
 const Dashboard = () => {
   const [user, loading, error] = useAuthState(auth);
   const navigate = useNavigate();
+  // console.log("user", user);
+
+  const usersUrl = `http://localhost:5000/api/v1/donors/${user?.email}`;
+  const {
+    isLoading,
+    error: dataError,
+    data,
+  } = useQuery("allusers", () => fetch(usersUrl).then((res) => res.json()));
+
+  console.log(data);
+
   return (
     <div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 mx-2">
@@ -56,6 +68,20 @@ const Dashboard = () => {
           </figure>
           <div class="card-body items-center text-center">
             <h2 class="card-title">Log Out</h2>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex justify-center items-center mt-12">
+        <div
+          onClick={() => navigate("/dashboard/users")}
+          class="card w-96 bg-red-500 shadow-xl text-white"
+        >
+          <figure class="px-10 pt-10">
+            <i class="fa-solid fa-users-between-lines"></i>
+          </figure>
+          <div class="card-body items-center text-center">
+            <h2 class="card-title">Manage Users</h2>
           </div>
         </div>
       </div>
